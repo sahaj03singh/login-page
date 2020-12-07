e577dc27807535f94589d85f479dfd8017f42233
sahaj15 <myemailid@domain.com>
2020-12-07 16:25:44 +0530

    login page

---
+++ fashiondesign/android/app/src/main/kotlin/com/example/fashiondesign/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fashiondesign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+          child:Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                child: Text(
+                  'Hello',
+                  style: TextStyle(
+                    fontSize: 80.0,
+                    fontWeight: FontWeight.bold)
+                  ),
+                ),
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 175.0, 0.0, 0.0),
+                child: Text(
+                   'There',
+                   style: TextStyle(
+                     fontSize: 80.0,
+                     fontWeight: FontWeight.bold)
+                   ),
+                ),
+              Container(
+                padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+                child: Text(
+                  '.',
+                  style: TextStyle(
+                    fontSize: 80.0,
+                    fontWeight: FontWeight.bold,
+                     color: Colors.green)
+                  ),
+
+                )
+
+            ],
+          ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top:35.0,left:20.0,right: 20.0),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'EMAIL',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Monserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color:Colors.green)
+                    )
+
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'PASSWORD',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Monserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color:Colors.green)
+                      )
+
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 5.0),
+                Container(
+                  alignment: Alignment(1.0,0.0),
+                  padding: EdgeInsets.only(top: 15.0,left: 20.0),
+                  child: InkWell(
+                    child: Text('Forgot Password?',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat',
+                      decoration: TextDecoration.underline
+                    ),),
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Container(
+                  height: 40.0,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20.0),
+                      shadowColor: Colors.greenAccent,
+                      color: Colors.green,
+                      elevation: 7.0,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Center(
+                          child: Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily:'Montserrat'
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  height: 40.0,
+                    color: Colors.transparent,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color:Colors.black,
+                        style: BorderStyle.solid,
+                        width: 1.0
+                      ),
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(20.0)
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: ImageIcon(AssetImage('assets/facebook.png')),
+                        ),
+                        SizedBox(width: 10.0),
+                        Center(
+                          child: Text('Log in with facebook',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat'
+                          )),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+          ),
+          SizedBox(height: 30.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('New to Spotify?',
+              style:TextStyle(
+                fontFamily: 'Montserrat'
+              ),),
+              SizedBox(width: 5.0),
+              InkWell(
+                onTap: (){},
+               child:Text('Register',
+                 style: TextStyle(
+                     color: Colors.green,
+                     fontFamily: 'Montserrat',
+                     fontWeight: FontWeight.bold,
+                     decoration: TextDecoration.underline
+                 ),
+
+               ),)
+            ],
+          )
+        ],
+      )
+
+    );
+  }
+}
